e64e2a63ec429c0c1171dcd82e41d6fa80d83a58
akfangus <akfanakfan@naver.com>
2023-04-25 04:15:52 +0900

    # FEAT : 기능
    ## Markdown 보이기
    
    1. react-markdown 라이브러리 사용
    2. syntax highlight로 코드부분 마크다운적용
    3. use client 넣어야 동작
    4. 외부url 에서 불러오는 이미지 config설정

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,4 +1,7 @@
+import MarkdownViewer from "@/components/MarkdownViewer";
 import { getPostData } from "@/service/posts";
+import Image from "next/image";
+import { AiTwotoneCalendar } from "react-icons/ai";
 
 type Props = {
   params: {
@@ -8,14 +11,30 @@ type Props = {
 
 const Postpage = async ({ params: { slug } }: Props) => {
   //1.전달된 slug에 해당하는 포스트데이터를 가져옴
-  //2. 그 데이터를 markdown 뷰어에 넘김
-
-  const post = await getPostData(slug);
+  //2. 그 데이터를 markdown 뷰어에 넘김(content)
+  //   const post = await getPostData(slug);
+  const { title, description, date, path, content } = await getPostData(slug);
   return (
-    <>
-      <h1>{post.title}</h1>
-      <pre>{post.content}</pre>
-    </>
+    <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-5">
+      <Image
+        className="w-full h-1/5 max-h-[500px] rounded-2xl"
+        src={`/images/posts/${path}.png`}
+        alt={title}
+        width={760}
+        height={420}
+      />
+      <section className="flex flex-col p-4">
+        <div className="flex items-center self-end text-sky-600">
+          <AiTwotoneCalendar />
+          <p className="font-semibold ml-2">{date.toString()}</p>
+        </div>
+        <h1 className="text-4xl font-bold">{title}</h1>
+        <p className="text-xl font-semibold">{description}</p>
+
+        <div className="w-52 border-2 border-sky-600 mt-4 mb-8" />
+        <MarkdownViewer content={content} />
+      </section>
+    </article>
   );
 };
 
